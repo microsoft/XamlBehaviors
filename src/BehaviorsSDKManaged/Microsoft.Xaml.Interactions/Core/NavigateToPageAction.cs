fef009b8c745d2b4c66a18c73a1725be01cf1e57
Jon Fortescue <t-jonf@microsoft.com>
2015-09-04 15:33:18 -0700

    Correcting error in namespace (Microsoft.Xaml.Interactions --> Microsoft.Xaml.Interactions.Core) :smirk:

--- src/BehaviorsSDKManaged/Microsoft.Xaml.Interactions/Core/NavigateToPageAction.cs
+++ src/BehaviorsSDKManaged/Microsoft.Xaml.Interactions/Core/NavigateToPageAction.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-namespace Microsoft.Xaml.Interactions
+namespace Microsoft.Xaml.Interactions.Core
 {
     using Windows.UI.Xaml;
     using Windows.UI.Xaml.Controls;
