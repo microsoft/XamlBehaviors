6cb25f827e7ce57a7a252ee389cd895e9b6f8785
Drew Gillies <drewgil@microsoft.com>
2015-11-17 14:21:58 -0800

    Collecting all assembly and package version files together in a Version directory in each source tree.

---
+++ src/BehaviorsSDKManaged/Version/Version.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved. 
+// Licensed under the MIT license. See LICENSE file in the project root for full license information. 
+
+using System.Reflection;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/BehaviorsSDKNative/Microsoft.Xaml.Interactivity.Design/Properties/AssemblyInfo.cs
+++ src/BehaviorsSDKNative/Microsoft.Xaml.Interactivity.Design/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 using Microsoft.Windows.Design.Metadata;
@@ -42,19 +40,6 @@ using Microsoft.Xaml.Interactivity.Design;
                                       // app, or any theme specific resource dictionaries)
 )]
 
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Entry point for metadata provider
 [assembly: ProvideMetadata(typeof(MetadataTableProvider))]
