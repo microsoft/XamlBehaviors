bb3d1e465f6e7056099fc5a84e2d5f34e549f7f5
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2020-02-27 15:07:15 -0800

    Using correct ICommand version.

--- src/BehaviorsSDKManaged/Microsoft.Xaml.Interactions/Core/InvokeCommandAction.cs
+++ src/BehaviorsSDKManaged/Microsoft.Xaml.Interactions/Core/InvokeCommandAction.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System.Windows.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Microsoft.Xaml.Interactions.Core
