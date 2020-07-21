3fbd63df30f027adcd47f1cd733a2d15498f9dd1
Jerome Laban <jerome.laban@nventive.com>
2020-07-21 08:56:14 -0400

    chore: Update to uno latest

--- samples/CS/XamlBehaviorsSample.Wasm/WasmScripts/GoogleAnalytics.d.ts
+++ samples/CS/XamlBehaviorsSample.Wasm/WasmScripts/GoogleAnalytics.d.ts
@@ -2,6 +2,6 @@ declare namespace Uno.UI.Demo {
     class Analytics {
         private static isLoaded;
         static reportPageView(screenName: string, appName?: string): string;
-        private static init(screenName, appName);
+        private static init;
     }
 }
