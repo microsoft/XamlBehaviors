88c8fd6e562517685efd516f34d9efafa868d51f
Daniel Vaughan <dbvaughan@gmail.com>
2020-09-08 21:25:57 -0700

    Update version to 1.0.6

--- src/BehaviorsSDKNative/Version/version.h
+++ src/BehaviorsSDKNative/Version/version.h
@@ -1,4 +1,4 @@
 // Behaviors SDK version for native dlls
 
-#define BEHAVIORS_VERSION_STRING "1.0.5.0"
-#define BEHAVIORS_VERSION_COMMA 1,0,5,0
+#define BEHAVIORS_VERSION_STRING "1.0.6.0"
+#define BEHAVIORS_VERSION_COMMA 1,0,6,0
