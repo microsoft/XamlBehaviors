62421c9988af07a424a6c4671ee5508dda73a39c
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2024-11-19 17:42:51 -0800

    Update all Sample C# files to file-scoped namespaces (and clean-up usings)

--- samples/CS/XAMLBehaviorsSample/ContentDialogSample.xaml.cs
+++ samples/CS/XAMLBehaviorsSample/ContentDialogSample.xaml.cs
@@ -1,32 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using Windows.UI.Xaml.Controls;
 
-// The Content Dialog item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+namespace XAMLBehaviorsSample;
 
-namespace XAMLBehaviorsSample
+public sealed partial class ContentDialogSample : ContentDialog
 {
-    public sealed partial class ContentDialogSample : ContentDialog
+    public ContentDialogSample()
     {
-        public ContentDialogSample()
-        {
-            this.InitializeComponent();
-        }
+        this.InitializeComponent();
+    }
 
-        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
-        {
-            this.Hide();
-        }
+    private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+    {
+        this.Hide();
     }
 }
--- samples/CS/XAMLBehaviorsSample/IncrementalUpdateControl.xaml.cs
+++ samples/CS/XAMLBehaviorsSample/IncrementalUpdateControl.xaml.cs
@@ -1,47 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using System.Collections.Generic;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+namespace XAMLBehaviorsSample;
 
-namespace XAMLBehaviorsSample
+public sealed partial class IncrementalUpdateControl : UserControl
 {
-    public sealed partial class IncrementalUpdateControl : UserControl
+    public List<ItemSample> Items;
+    public IncrementalUpdateControl()
     {
-        public List<ItemSample> Items;
-        public IncrementalUpdateControl()
+        this.InitializeComponent();
+        Items = new List<ItemSample>();
+        gridViewSample.ItemsSource = Items;
+        for(int i = 1; i <= 100; i++)
         {
-            this.InitializeComponent();
-            Items = new List<ItemSample>();
-            gridViewSample.ItemsSource = Items;
-            for(int i = 1; i <= 100; i++)
-            {
-                Items.Add(new ItemSample(i));
-            }
+            Items.Add(new ItemSample(i));
         }
     }
+}
 
-    public class ItemSample
+public class ItemSample
+{
+    public int Count { get; set; }
+    public ItemSample()
     {
-        public int Count { get; set; }
-        public ItemSample()
-        {
-            Count = 0;
-        }
-        public ItemSample(int i)
-        {
-            Count = i;
-        }
+        Count = 0;
+    }
+    public ItemSample(int i)
+    {
+        Count = i;
     }
 }
