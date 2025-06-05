6570fdef1bf728d4c032947565f7e641033e38a6
Charles Milette <charles.milette@gmail.com>
2025-06-04 20:29:38 -0400

    Add new trim-safe EventTriggerBehaviorBase<T>, rewrite EventTriggerBehavior to be based upon trim-safe base, add various common event-based behaviors

---
+++ src/BehaviorsSDKManaged/Microsoft.Xaml.Interactivity.Shared/Core/PointerEnteredTriggerBehavior.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#if WinUI
+using Microsoft.UI.Xaml;
+#else
+using Windows.UI.Xaml;
+#endif
+
+namespace Microsoft.Xaml.Interactivity;
+
+/// <summary>
+/// A behavior that listens to an <see cref="UIElement"/> and executes its actions when the pointer enters the UI element.
+/// </summary>
+public class PointerEnteredTriggerBehavior : EventTriggerBehaviorBase<UIElement>
+{
+    /// <inheritdoc/>
+    protected override bool RegisterEventCore(UIElement source)
+    {
+        source.PointerEntered += OnEvent;
+        return true;
+    }
+
+    /// <inheritdoc/>
+    protected override void UnregisterEventCore(UIElement source)
+    {
+        source.PointerEntered -= OnEvent;
+    }
+}
